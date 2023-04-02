ba304ac5f952b77317f54e797e8312767eb8f1e0
cmccune2 <98060977+cmccune2@users.noreply.github.com>
2023-04-02 15:20:02 -0400

    a8 now working without link

--- src/services/tuits-service.js
+++ src/services/tuits-service.js
@@ -1,6 +1,10 @@
 import axios from 'axios';
-const API_BASE = 'https://tuiter-node-server-app-921u.onrender.com/api';
+// const API_BASE = 'https://tuiter-node-server-app-921u.onrender.com/api';
+// const TUITS_API = `${API_BASE}/tuits`;
+
+const API_BASE = process.env.REACT_APP_API_BASE;
 const TUITS_API = `${API_BASE}/tuits`;
+
 
 export const createTuit = async (tuit) => {
     const response = await axios.post(TUITS_API, tuit)
