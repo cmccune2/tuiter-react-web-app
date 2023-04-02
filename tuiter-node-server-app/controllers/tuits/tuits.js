bb65bb6789d1286b464bed75ac99d770cb3544b8
cmccune2 <98060977+cmccune2@users.noreply.github.com>
2023-04-02 13:36:51 -0400

    a8 working

--- tuiter-node-server-app/controllers/tuits/tuits.js
+++ tuiter-node-server-app/controllers/tuits/tuits.js
@@ -6,6 +6,7 @@ export default [
         "tuit": "You want to wake up in the morning and think the future is going to be great - and that’s what being a spacefaring civilization is all about. It’s about believing in the future and thinking that the future will be better than the past. And I can’t think of anything more exciting than going out there and being among the stars",
         "liked": true, "likes": 2345, "replies": 123, "retuits": 432
     },
+
     {
         "_id": "234", "topic": "Traffic", "username": "The Boring Company",
         "handle": "@boringcompany", "time": "2h", "image": "tbc.png",
